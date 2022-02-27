956fbed92625d7a3b8e6b604a42c53b6b028b0fd
Yuri Corredor <YuriCorredor159@gmail.com>
2022-02-27 15:52:06 -0300

    created world and imported static and animated models

--- Learning/Learning TheeJs/src/script.js
+++ Learning/Learning TheeJs/src/script.js
@@ -1,110 +1,172 @@
-import * as THREE from 'three'
-import classes from './style.css'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+    import * as THREE from 'three'
+    import classes from './style.css'
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-let renderer, scene, camera, mixers, controls, previousRAF
+    let renderer, scene, camera, controls, previousRAF
+    let mixers = []
 
-const main = () => {
-    initializeWorld()
-    initializeOrbitControls()
-    initializeGround()
-    RAF() //request animation frame --updates
-}
+    const main = () => {
+        initializeWorld()
+        initializeGround()
+        initializeOrbitControls()
+        //loadStaticModel('./models/car/car.gltf', 15)
+        loadAnimatedModel('./models/xbot/', 'xbot.fbx', 0.5, 'dancing.fbx')
+        RAF() //request animation frame --updates
+    }
 
-const initializeWorld = () => {
-    //initializing the renderer
-    const canvas = document.querySelector('canvas.webgl')
-    renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas})
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    const initializeWorld = () => {
+        //initializing the renderer
+        const canvas = document.querySelector('canvas.webgl')
+        renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas})
+        renderer.shadowMap.enabled = true
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight)
 
-    //initializing the camera
-    const fov = 60
-    const aspect = window.innerWidth / window.innerHeight
-    const near = 1.0
-    const far = 1000.0
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-    camera.position.set(90, 20, 0)
+        //initializing the camera
+        const fov = 75
+        const aspect = window.innerWidth / window.innerHeight
+        const near = 1.0
+        const far = 1000.0
+        camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+        camera.position.set(0, 20, 85)
 
-    window.addEventListener('resize', () => {
-        onWindowResize()
-    }, false)
+        //adding eventListner on resizing to matain the aspect ratio
+        window.addEventListener('resize', () => {
+            onWindowResize()
+        }, false)
 
-    //initializing the scene
-    scene = new THREE.Scene()
+        //initializing the scene
+        scene = new THREE.Scene()
 
-    //adding lighting to the scene
-    let hemisphereLight = new THREE.HemisphereLight( 0xffffff, 0x444444 )
-    hemisphereLight.position.set( 0, 300, 0 )
-    scene.add( hemisphereLight )
+        //adding lighting to the scene
+        const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
+        scene.add(ambientLight)
 
-    let directionalLight = new THREE.DirectionalLight( 0xffffff )
-    directionalLight.position.set( 75, 300, -75 )
-    scene.add( directionalLight )
+        const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1.5)
+        hemisphereLight.position.set( 75, 300, 0 )
+        scene.add(hemisphereLight)
 
-    //loading texture and adding it to the background
-    const loader = new THREE.CubeTextureLoader()
-    const texture = loader.load([
-        'posx.bmp',
-        'negx.bmp',
-        'posy.bmp',
-        'negy.bmp',
-        'posz.bmp',
-        'negz.bmp'
-    ])
-    scene.background = texture
-}
 
-const initializeOrbitControls = () => {
-    controls = new OrbitControls(camera, renderer.domElement)
-    controls.target.set(0, 20, 0)
-    controls.update()
-}
+        //adding helper to one light
+        /*
+        hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 10)
+        hemisphereLight.position.set( 0, 10, 0 )
+        let cameraHelper = new THREE.HemisphereLightHelper(hemisphereLight, 2)
+        hemisphereLight.add(cameraHelper)
+        scene.add(hemisphereLight)
+        */
 
-const initializeGround = () => {
-    const ground = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(100, 100, 10),
-        new THREE.MeshStandardMaterial()
-    )
-    ground.castShadow = false
-    ground.receiveShadow = true
-    ground.rotation.x = -Math.PI / 2
-    scene.add(ground)
-}
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+        directionalLight.position.set(75, 300, 0)
+        scene.add(directionalLight)
+        
+        //loading texture and adding it to the background
+        const loader = new THREE.CubeTextureLoader()
+        const texture = loader.load([
+            './background/posx.bmp',
+            './background/negx.bmp',
+            './background/posy.bmp',
+            './background/negy.bmp',
+            './background/posz.bmp',
+            './background/negz.bmp'
+        ])
+        scene.background = texture
+    }
 
-const RAF = () => {
-    requestAnimationFrame(t => {
-        if (previousRAF === null) {
+    const initializeOrbitControls = () => {
+        controls = new OrbitControls(camera, renderer.domElement)
+        controls.target.set(0, 20, 0)
+        controls.update()
+    }
+
+    const initializeGround = () => {
+        const ground = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(100, 100, 1),
+            new THREE.MeshStandardMaterial()
+        )
+        ground.castShadow = false
+        ground.receiveShadow = true
+        ground.rotation.x = -Math.PI / 2
+        scene.add(ground)
+    }
+
+    const loadStaticModel = (modelPath, scale) => {
+        const gltfLoader = new GLTFLoader()
+        gltfLoader.load(modelPath, gltf => {
+            gltf.scene.traverse(c => {
+                c.receiveShadow = true
+                c.castShadow = true
+                if (c.isMesh) {
+                    if (c.material.map) c.material.map.anisotropy = 16
+                }
+            })
+            gltf.scene.scale.setScalar(scale)
+            scene.add(gltf.scene)
+        })
+    }   
+
+    const loadAnimatedModel = (modelPath, model, scale, animation) => {
+        //initializing the loader
+        const fbxLoader = new FBXLoader()
+        fbxLoader.setPath(modelPath)
+
+        //loading the model
+        fbxLoader.load(model, fbx => {
+            fbx.traverse(c => {
+                c.castShadow = true
+                c.receiveShadow = true
+                if (c.isMesh) {
+                    if (c.material.map) c.material.map.anisotropy = 16
+                }
+            })
+            fbx.scale.setScalar(scale);
+            fbx.position.y = 4 
+
+            //loading the animation
+            fbxLoader.load(animation, anim => {
+                const mixer = new THREE.AnimationMixer(fbx)
+                mixers.push(mixer)
+                const idle = mixer.clipAction(anim.animations[0])
+                idle.play()
+            })
+            scene.add(fbx)
+        })
+
+    }
+
+    const RAF = () => {
+        requestAnimationFrame(t => {
+            if (previousRAF === null) {
+                previousRAF = t
+            }
+
+            renderer.render(scene, camera)
+
+            RAF()
+
+            step(t - previousRAF)
             previousRAF = t
+        })
+
+        const step = timeElapsed => {
+            const timeElapsedSeconds = timeElapsed / 1000
+            if (mixers) {
+                mixers.map(m => m.update(timeElapsedSeconds))
+            }
+        
+            if (controls) {
+                controls.update(timeElapsedSeconds)
+            }
         }
-
-        renderer.render(scene, camera)
-        step(t - previousRAF)
-        previousRAF = t
-  
-        RAF()
-    })
-}
-
-const step = timeElapsed => {
-    const timeElapsedSeconds = timeElapsed / 1000
-    if (mixers) {
-        mixers.map(m => m.update(timeElapsedSeconds))
     }
 
-    if (controls) {
-        controls.update(timeElapsedSeconds)
+    const onWindowResize = () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
     }
-}
 
-const onWindowResize = () => {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-}
-
-window.addEventListener('DOMContentLoaded', () => main())
+    window.addEventListener('DOMContentLoaded', () => main())
