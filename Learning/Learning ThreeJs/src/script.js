f67f3d2f6b450d8879bfc2144ddcc3e49e80b4b6
Yuri Corredor <YuriCorredor159@gmail.com>
2022-02-28 20:49:08 -0300

    controls are now working and animations are in place

--- Learning/Learning ThreeJs/src/script.js
+++ Learning/Learning ThreeJs/src/script.js
@@ -144,103 +144,155 @@ const loadAnimatedModelWithControls = () => {
         camera: camera,
         scene: scene
     }
-    controls = initializeCharacterController(
-        params,
-        './models/xbot/',
-        'xbot.fbx',
-        ['dancing.fbx', 'running.fbx', 'walking.fbx']
-    )
+    controls = new BasicCharacterController(params)
 }
 
-const initializeCharacterController = (params, modelPath, model, animationsNames) => {
-
-    let decceleration, acceleration, velocity, animations, input, stateMachine, target, mixer, manager
-
-    decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0)
-    acceleration = new THREE.Vector3(1, 0.25, 50.0)
-    velocity = new THREE.Vector3(0, 0, 0)
-
-    animations = {}
-    input = basicCharacterControllerInput()
-    stateMachine = new CharacterFSM(animations)
-
-    //loading models
-    const loader = new FBXLoader()
-    loader.setPath(modelPath)
-    loader.load(model, fbx => {
-        fbx.scale.setScalar(0.1);
-        fbx.traverse(c => {
-            c.castShadow = true
-            c.receiveShadow = true
-            if (c.isMesh) {
-                if (c.material.map) c.material.map.anisotropy = 16
-            }
-        })
-
-        target = fbx
-        params.scene.add(target)
-
-        mixer = new THREE.AnimationMixer(target)
-
-        manager = new THREE.LoadingManager()
-        manager.onLoad = () => {
-            stateMachine.SetState('idle')
-        }
-
-        const onLoad = (animationName, anim) => {
-            const clip = anim.animations[0]
-            const action = mixer.clipAction(clip)
-        
-            animations[animationName] = {
-                clip: clip,
-                action: action,
-            }
-        }
-
-        const loader = new FBXLoader(manager);
-        loader.setPath(modelPath)
-        animationsNames.forEach(animationName => {
-            loader.load(animationName, anim => onLoad(animationName, anim))
-        })
-
-    })
-    
-}
-
-const basicCharacterControllerInput = () => {
-    let keys
-
-    keys = {
-        forward: false,
-        backward: false,
-        left: false,
-        right: false,
-        space: false,
-        shift: false
+class BasicCharacterController {
+    constructor(params) {
+        this._Init(params)
     }
 
-    document.addEventListener('keydown', event => {
-        if (event.code === 'KeyW') keys.forward = true
-        if (event.code === 'KeyA') keys.left = true
-        if (event.code === 'KeyD') keys.right = true
-        if (event.code === 'KeyS') keys.backward = true
-        if (event.code === 'Space') keys.space = true
-        if (event.code === 'ShiftLeft') keys.shift = true
-    }, false)
+    _Init(params) {
 
-    document.addEventListener('keyup', event => {
-        if (event.code === 'KeyW') keys.forward = false
-        if (event.code === 'KeyA') keys.left = false
-        if (event.code === 'KeyD') keys.right = false
-        if (event.code === 'KeyS') keys.backward = false
-        if (event.code === 'Space') keys.space = false
-        if (event.code === 'ShiftLeft') keys.shift = false
-    }, false)
+        this._params = params
+        this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0)
+        this._acceleration = new THREE.Vector3(1, 0.25, 50.0)
+        this._velocity = new THREE.Vector3(0, 0, 0)
 
-    return keys
+        this._animations = {}
+        this._input = new BasicCharacterControllerInput()
+        this._stateMachine = new CharacterFSM(
+            new BasicCharacterControllerProxy(this._animations)
+        )
+
+        this._LoadModels()
+    }
+
+    _LoadModels() {
+        const loader = new FBXLoader()
+        loader.setPath('./models/xbot/')
+        loader.load('xbot.fbx', (fbx) => {
+            fbx.scale.setScalar(0.1)
+            fbx.traverse(c => {
+                c.castShadow = true
+                c.receiveShadow = true
+                if (c.isMesh) {
+                    if (c.material.map) c.material.map.anisotropy = 16
+                }
+            })
+            this._target = fbx
+            this._params.scene.add(this._target)
+            this._mixer = new THREE.AnimationMixer(this._target)
+            this._manager = new THREE.LoadingManager()
+            this._manager.onLoad = () => {
+                this._stateMachine.SetState('idle')
+            }
+
+            const _OnLoad = (animName, anim) => {
+                const clip = anim.animations[0]
+                const action = this._mixer.clipAction(clip)
+        
+                this._animations[animName] = {
+                    clip: clip,
+                    action: action,
+                }
+            }
+            const loader = new FBXLoader(this._manager)
+            loader.setPath('./models/xbot/')
+            loader.load('walk.fbx', (a) => _OnLoad('walk', a))
+            loader.load('run.fbx', (a) => _OnLoad('run', a))
+            loader.load('idle.fbx', (a) => _OnLoad('idle', a))
+            loader.load('dance.fbx', (a) => _OnLoad('dance', a))
+        })
+    }
+
+    Update(timeInSeconds) {
+        if (!this._target || !this._stateMachine._currentState) {
+            return;
+          }
+          this._stateMachine.Update(timeInSeconds, this._input);
+          const velocity = this._velocity;
+          const frameDecceleration = new THREE.Vector3(
+              velocity.x * this._decceleration.x,
+              velocity.y * this._decceleration.y,
+              velocity.z * this._decceleration.z
+          );
+          frameDecceleration.multiplyScalar(timeInSeconds);
+          frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+              Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+      
+          velocity.add(frameDecceleration);
+      
+          const controlObject = this._target;
+          const _Q = new THREE.Quaternion();
+          const _A = new THREE.Vector3();
+          const _R = controlObject.quaternion.clone();
+      
+          const acc = this._acceleration.clone();
+          if (this._input._keys.shift) {
+            acc.multiplyScalar(2.0);
+          }
+      
+          if (this._stateMachine._currentState.Name == 'dance') {
+            acc.multiplyScalar(0.0);
+          }
+      
+          if (this._input._keys.forward) {
+            velocity.z += acc.z * timeInSeconds;
+          }
+          if (this._input._keys.backward) {
+            velocity.z -= acc.z * timeInSeconds;
+          }
+          if (this._input._keys.left) {
+            _A.set(0, 1, 0);
+            _Q.setFromAxisAngle(_A, 4.0 * Math.PI * timeInSeconds * this._acceleration.y);
+            _R.multiply(_Q);
+          }
+          if (this._input._keys.right) {
+            _A.set(0, 1, 0);
+            _Q.setFromAxisAngle(_A, 4.0 * -Math.PI * timeInSeconds * this._acceleration.y);
+            _R.multiply(_Q);
+          }
+      
+          controlObject.quaternion.copy(_R);
+      
+          const oldPosition = new THREE.Vector3();
+          oldPosition.copy(controlObject.position);
+      
+          const forward = new THREE.Vector3(0, 0, 1);
+          forward.applyQuaternion(controlObject.quaternion);
+          forward.normalize();
+      
+          const sideways = new THREE.Vector3(1, 0, 0);
+          sideways.applyQuaternion(controlObject.quaternion);
+          sideways.normalize();
+      
+          sideways.multiplyScalar(velocity.x * timeInSeconds);
+          forward.multiplyScalar(velocity.z * timeInSeconds);
+      
+          controlObject.position.add(forward);
+          controlObject.position.add(sideways);
+      
+          oldPosition.copy(controlObject.position);
+      
+          if (this._mixer) {
+            this._mixer.update(timeInSeconds);
+          }
+    }
 }
 
-//A normalized FiniteStateMachine. Can be used in many projects.
+//An proxy to CharacterFSM
+class BasicCharacterControllerProxy {
+    constructor(animations) {
+        this._animations = animations
+    }
+    
+    get animations() {
+        return this._animations
+    }
+}
+
+//A normal FiniteStateMachine. Can be used in many projects.
 class FiniteStateMachine {
     constructor() {
         this._states = {}
@@ -269,12 +321,12 @@ class FiniteStateMachine {
     }
 }
 
-//A extension of the FinateStateMachine personalized for our needs(adding the states with AddState)
+//An extension of the FinateStateMachine personalized for our needs(adding the states with AddState)
 class CharacterFSM extends FiniteStateMachine {
-    constructor(animations) {
-        super();
-        this._animations = animations;
-        this._Init();
+    constructor(proxy) {
+        super()
+        this._proxy = proxy
+        this._Init()
     }
 
     _Init() {
@@ -283,6 +335,61 @@ class CharacterFSM extends FiniteStateMachine {
         this._AddState('walk', WalkState)
         this._AddState('run', RunState)
         this._AddState('dance', DanceState)
+    }
+}
+
+class BasicCharacterControllerInput {
+    constructor() {
+        this._Init()
+    }
+
+    _Init() {
+        this._keys = {
+            forward: false,
+            backward: false,
+            left: false,
+            right: false,
+            space: false,
+            shift: false
+        }
+
+        document.addEventListener('keydown', event => this._onKeyDown(event), false)
+        document.addEventListener('keyup', event => this._onKeyUp(event), false)
+    }
+
+    _onKeyDown(event) {
+        switch(event.code) {
+            case 'KeyW': this._keys.forward = true
+            break
+            case 'KeyA': this._keys.left = true
+            break
+            case 'KeyD': this._keys.right = true
+            break
+            case 'KeyS': this._keys.backward = true
+            break
+            case 'Space': this._keys.space = true
+            break
+            case 'ShiftLeft': this._keys.shift = true
+            break
+        }
+    
+    }
+
+    _onKeyUp(event) {
+        switch(event.code) {
+            case 'KeyW': this._keys.forward = false
+            break
+            case 'KeyA': this._keys.left = false
+            break
+            case 'KeyD': this._keys.right = false
+            break
+            case 'KeyS': this._keys.backward = false
+            break
+            case 'Space': this._keys.space = false
+            break
+            case 'ShiftLeft': this._keys.shift = false
+            break
+        }
     }
 }
 
@@ -298,7 +405,7 @@ class State {
 }
 
 class IdleState extends State {
-    constructor() {
+    constructor(parent) {
         super(parent)
     }
 
@@ -307,44 +414,45 @@ class IdleState extends State {
     }
 
     Enter(prevState) {
-        console.log(this._parent)
-        const idleAction = this._parent.animations['idle'].action
+        const idleAction = this._parent._proxy._animations['idle'].action
         if (prevState) {
-            const prevAction = this._parent._animations[prevState.Name].action
+            const prevAction = this._parent._proxy._animations[prevState.Name].action
             idleAction.time = 0.0
             idleAction.enabled = true
             idleAction.setEffectiveTimeScale(1.0)
             idleAction.setEffectiveWeight(1.0)
             idleAction.crossFadeFrom(prevAction, 0.5, true)
             idleAction.play()
-        } else idleAction.play()
+        } else {
+            idleAction.play()
+        }
     }
 
     Update(_, input) {
-        if (input.forward || input.backward) this._parent.SetState('walk')
-        else if (input.space) this._parent.SetState('dance')
+        if (input._keys.forward || input._keys.backward) this._parent.SetState('walk')
+        else if (input._keys.space) this._parent.SetState('dance')
     }
 }
 
 class WalkState extends State {
-    constructor() {
+    constructor(parent) {
         super(parent)
     }
-
+  
     get Name() {
         return 'walk'
     }
-
+  
     Enter(prevState) {
-        const curAction = this._parent._animations['walk'].action
+        const curAction = this._parent._proxy._animations['walk'].action
         if (prevState) {
-            const prevAction = this._parent_animations[prevState.Name].action
+            const prevAction = this._parent._proxy._animations[prevState.Name].action
             curAction.enabled = true
             if (prevState.Name == 'run') {
                 const ratio = curAction.getClip().duration / prevAction.getClip().duration
                 curAction.time = prevAction.time * ratio
             } else {
-                curAction.time = 0.0;
+                curAction.time = 0.0
                 curAction.setEffectiveTimeScale(1.0)
                 curAction.setEffectiveWeight(1.0)
             }
@@ -354,10 +462,10 @@ class WalkState extends State {
             curAction.play()
         }
     }
-
+  
     Update(_, input) {
-        if (input.forward || input.backward) {
-            if (input.shift) {
+        if (input._keys.forward || input._keys.backward) {
+            if (input._keys.shift) {
                 this._parent.SetState('run')
             }
             return
@@ -367,7 +475,7 @@ class WalkState extends State {
 }
 
 class RunState extends State {
-    constructor() {
+    constructor(parent) {
         super(parent)
     }
 
@@ -396,8 +504,8 @@ class RunState extends State {
     }
 
     Update(_, input) {
-        if (input.forward || input.backward) {
-            if (!input.shift) {
+        if (input._keys.forward || input._keys.backward) {
+            if (!input._keys.shift) {
                 this._parent.SetState('walk')
             }
             return
@@ -407,7 +515,7 @@ class RunState extends State {
 }
 
 class DanceState extends State {
-    constructor() {
+    constructor(parent) {
         super(parent)
     }
 
@@ -420,12 +528,12 @@ class DanceState extends State {
             this._Cleanup()
             this._parent.SetState('idle')
         }
-        const curAction = this._parent._animations['dance'].action
+        const curAction = this._parent._proxy._animations['dance'].action
         const mixer = curAction.getMixer()
         mixer.addEventListener('finished', finishedCallback)
 
         if (prevState) {
-            const prevAction = this._parent._animations[prevState.Name].action
+            const prevAction = this._parent._proxy._animations[prevState.Name].action
             curAction.reset()
             curAction.setLoop(THREE.LoopOnce, 1)
             curAction.clampWhenFinished = true
@@ -441,7 +549,7 @@ class DanceState extends State {
     }
 
     _Cleanup() {
-        const action = this._parent._animations['dance'].action
+        const action = this._parent._proxy._animations['dance'].action
         action.getMixer().removeEventListener('finished')
     }
 }
@@ -453,11 +561,10 @@ const RAF = () => {
         }
 
         renderer.render(scene, camera)
-
-        RAF()
-
         step(t - previousRAF)
         previousRAF = t
+
+        RAF()
     })
 
     const step = timeElapsed => {
@@ -467,7 +574,7 @@ const RAF = () => {
         }
     
         if (controls) {
-            controls.update(timeElapsedSeconds)
+            controls.Update(timeElapsedSeconds)
         }
     }
 }
