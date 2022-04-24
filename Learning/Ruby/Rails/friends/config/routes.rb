04a09bd7ee4f601c394b42da021843168b3d0358
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-24 10:08:23 -0300

    'rails g scaffold name:string email:string phone:string' to create a crud base with a db migration, model in 'app/models', controller, views and so on.

--- Learning/Ruby/Rails/friends/config/routes.rb
+++ Learning/Ruby/Rails/friends/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :friends
+  #get 'home/index'
+  root 'home#index'
+  get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ Learning/Ruby/Rails/friends/db/migrate/20220424130250_create_friends.rb
@@ -0,0 +1,11 @@
+class CreateFriends < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friends do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
