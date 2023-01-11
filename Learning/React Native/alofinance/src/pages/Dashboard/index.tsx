2c5e5a49f2bd8f1d5ba1452b484f755ad3fb72c0
YuriCorredor <YuriCorredor159@gmail.com>
2023-01-11 20:06:06 -0300

    feat: :sparkles: add react-native

---
+++ Learning/React Native/alofinance/src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Container, Title } from "./styles"
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Title>ABCD</Title>
+    </Container>
+  )
+}
---
+++ Learning/React Native/alofinance/src/pages/Dashboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #ccc;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+`
+
