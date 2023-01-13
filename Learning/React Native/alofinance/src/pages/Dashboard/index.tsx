2c0d298ffd5a595fdad6c9629ceaca43cd82d740
YuriCorredor <YuriCorredor159@gmail.com>
2023-01-12 22:15:39 -0300

    feat: :sparkles: add Dashboard page

--- Learning/React Native/alofinance/src/pages/Dashboard/index.tsx
+++ Learning/React Native/alofinance/src/pages/Dashboard/index.tsx
@@ -1,10 +1,60 @@
 import React from "react"
-import { Container, Title } from "./styles"
+import HighlightCard from "../../components/HighlightCard"
+import TransactionCard from "../../components/TransactionCard"
+import {
+  Container,
+  Header,
+  HighlightCards,
+  Icon,
+  Title,
+  Transactions,
+  UserGreating,
+  UserHello,
+  UserImage,
+  UserInfo,
+  UserInfoContainer,
+  UserName,
+} from "./styles"
 
 export default function Dashboard() {
   return (
     <Container>
-      <Title>ABCD</Title>
+      <Header>
+        <UserInfoContainer>
+          <UserInfo>
+            <UserImage source={{ uri: 'https://avatars.githubusercontent.com/u/91801065?v=4' }} />
+            <UserGreating>
+              <UserHello>Olá,</UserHello>
+              <UserName>Corredor</UserName>
+            </UserGreating>
+          </UserInfo>
+          <Icon name="power" />
+        </UserInfoContainer>
+      </Header>
+      <HighlightCards>
+        <HighlightCard 
+          title="Entradas"
+          amount="17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="16.141,00"
+          lastTransaction="01 à 06 de abril"
+          type="total"
+        />
+      </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   )
 }
--- Learning/React Native/alofinance/src/pages/Dashboard/styles.ts
+++ Learning/React Native/alofinance/src/pages/Dashboard/styles.ts
@@ -1,15 +1,81 @@
 import styled from "styled-components/native"
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize"
+import { Feather } from "@expo/vector-icons"
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-  align-items: center;
+`
+
+export const Header = styled.SafeAreaView`
+  background-color: ${({ theme}) => theme.colors.primary};
+  padding: ${RFPercentage(8)}px ${RFPercentage(3)}px;
+  height: ${RFPercentage(42)}px;
+  align-items: flex-start;
   justify-content: center;
+  flex-direction: row;
+`
+
+export const UserInfoContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const UserInfo = styled.View`
+  flex: 1;
+  flex-direction: row;
+`
+
+export const UserImage = styled.Image`
+  width: ${RFValue(55)}px;
+  height: ${RFValue(55)}px;
+  border-radius: 10px;
+`
+
+export const UserGreating = styled.View`
+  flex: 1;
+  flex-direction: column;
+  padding-left: ${RFPercentage(2)}px;
+`
+
+export const UserHello = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFPercentage(3)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`
+
+export const UserName = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFPercentage(3)}px;
+  color: ${({ theme }) => theme.colors.shape};
 `
 
 export const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.title};
+  margin-bottom: ${RFValue(12)}px;
 `
 
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingRight:  RFValue(16) }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 ${RFPercentage(3)}px;
+  margin-top: ${RFPercentage(12)}px;
+`
