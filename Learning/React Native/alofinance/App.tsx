a0e7d28fde62ee29dbfa3730fbbd2b36537ebcbd
YuriCorredor <YuriCorredor159@gmail.com>
2023-01-12 22:12:52 -0300

    feat: :sparkles: add font

--- Learning/React Native/alofinance/App.tsx
+++ Learning/React Native/alofinance/App.tsx
@@ -1,11 +1,36 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins"
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from "react"
+import { View } from "react-native"
 import { ThemeProvider } from "styled-components"
 import theme from "./src/global/styles/theme"
 import Dashboard from "./src/pages/Dashboard"
 
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_700Bold })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Dashboard />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+          <Dashboard />
+        </View>
+      </ThemeProvider>
   );
 }
