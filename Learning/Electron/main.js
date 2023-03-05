b8a8fa87d74060c538fb9e5c04e57d6850b8b204
unknown <yuri.corredor@focusnfe.com.br>
2023-03-04 22:42:15 -0300

    feat: :sparkles: electron

---
+++ Learning/Electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  ipcMain.handle('ping', () => 'pong')
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
