92ffb28a12de11733434fae4df29b5ec6fbdaf65
YuriCorredorFocus <yuri.corredor@focusnfe.com.br>
2023-11-19 19:06:47 -0300

    feat: :sparkles: binary search

--- Learning/Algorithms/src/day1/BinarySearchList.ts
+++ Learning/Algorithms/src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let left = 0
+  let right = haystack.length - 1
 
+  while (right >= left) {
+    let middle = Math.floor((left + right) / 2)
+    const guess = haystack[middle]
+
+    if (guess === needle) {
+      return true
+    } else if (guess < needle) {
+      left = middle + 1
+    } else if (guess > needle) {
+      right = middle - 1
+    }
+  }
+
+  return false
 }
+
--- Learning/Algorithms/src/day1/TwoCrystalBalls.ts
+++ Learning/Algorithms/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,38 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+/*
+Question:
+You have two crystal balls, and you want to determine the highest floor of an n-story building an egg can be dropped from without breaking.
+Each ball has the following properties:
+- If dropped from floor F or below, it will not break.
+- If dropped from floor F + 1 or above, it will break.
+- If dropped from any floor between F and F + 1, the result is unknown.
+What's the best strategy for determining F while dropping the smallest number of balls?
 
+Examples:
+Input: [false, false, false, false, true, true, true, true]
+Output: 4
+
+Input: [false, false, false, false, false, false, false, false]
+Output: -1
+
+Input: [true, true, true, true, true, true, true, true]
+Output: 0
+*/
+export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+  let floor = jumpAmount
+
+  for (let i = 0; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      floor = i
+      break
+    }
+  }
+
+  for (let i = floor - jumpAmount; i < floor; ++i) {
+    if (breaks[i]) {
+      return i
+    }
+  }
+
+  return -1
 }
