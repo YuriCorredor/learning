3474ade05e8b9c97213dc026318ac16328f039dc
YuriCorredorFocus <yuri.corredor@focusnfe.com.br>
2024-02-08 14:12:52 -0300

    Implement bubble sort algorithm

--- Learning/Algorithms/src/day1/BubbleSort.ts
+++ Learning/Algorithms/src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
+  let pointer = arr.length - 1
 
+  while (pointer != 1) {
+    for (let i = 0; i <= pointer ;i++) {
+      const left = arr[i]
+      const right = arr[i+1]
+
+      if (left > right) {
+        arr[i] = right
+        arr[i+1] = left
+      }
+    }
+
+    pointer -= 1
+  }
 }
